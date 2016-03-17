10abe373bc36457ce4ea28404f1ae4fb2abc965c
Kremenchuk <kremenchuk@bk.ru>
2016-03-17 22:55:26 +0200

    25.02.2016

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     #Выдача админских прав юзеру
-      #@user = User.where("email = 'kremenchuk@bk.ru'").first
-      #@user.admin = 1
-      #@user.save
+      @user = User.where("email = 'kremenchuk@bk.ru'").first
+      @user.admin = 1
+      @user.save
 
     button_const
   end
