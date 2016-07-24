165bc62f1053847a8b2cef0a1e9b6e958aeddce1
Kremenchuk <kremenchuk@bk.ru>
2016-07-24 15:06:11 +0300

    25.02.2016

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,14 +16,14 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'http://k-sklad.com.ua', port: '80' }
+  config.action_mailer.default_url_options = { host:'k-sklad.com.ua', port: '80' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'http://k-sklad.com.ua:80',
+      :domain => 'k-sklad.com.ua',
       :user_name => "kremartem@gmail.com",
       :password => "ufkfrnbrf810",
       :authentication => :plain,
