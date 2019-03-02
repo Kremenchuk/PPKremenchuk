85aa7150b2b20113584acd585b78f4c23bb91fb0
Kremenchuk <kremenchuk@bk.ru>
2019-03-02 14:41:18 +0200

    some change for SEO

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -26,13 +26,13 @@ class NewsController < ApplicationController
 
     begin
       image_name = "news_#{(Time.now).to_i}_#{params[:photo].original_filename}"
-      image_path = File.join(Rails.root,'public/assets/images/news')
+      image_path = File.join(Rails.root,'/public/assets/news')
 
       File.open(File.join(image_path, image_name),'wb') do |f|
         f.write(params[:photo].read)
       end
 
-      news.photo = File.join('/images/news', image_name)
+      news.photo = File.join('/assets/news', image_name)
 
       news.save!
     end
