7243ada6e15bb41a07ca5061ad86417f63ca2e24
Kremenchuk <ufkfrnbrf810>
2021-11-21 15:03:42 +0200

    some change

--- app/controllers/constants_controller.rb
+++ app/controllers/constants_controller.rb
@@ -14,6 +14,7 @@ class ConstantsController < ApplicationController
 
   def load_constant
     # Завантаження файлу з константами
+    #
     uploaded_io = params[:constant_file]
     if uploaded_io != nil
       begin
