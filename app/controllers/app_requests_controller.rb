48903d2e6d9c368f7a20d023feb3996377781064
Artem Kremenchuk <kremartem@gmail.com>
2024-08-27 20:49:40 +0300

    added middleware to log app requests

--- app/controllers/app_requests_controller.rb
+++
@@ -1,6 +0,0 @@
-class AppRequestsController < ApplicationController
-  
-  def index
-    @app_requests = AppRequest.where(created_at: (Time.now - 24.hours)..Time.now)
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require_relative '../app/middlewares/requests_logger'
-
 module Ppkremenchuk
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -20,6 +18,5 @@ module Ppkremenchuk
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.middleware.use RequestsLogger
   end
 end
