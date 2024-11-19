dab3895e38ab181485ac00e1c0f36fb70c87563a
Artem Kremenchuk <kremartem@gmail.com>
2024-11-19 10:24:46 +0200

    changed work with gallery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_17_094604) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_19_082410) do
   create_table "articles", force: :cascade do |t|
     t.json "title", default: {"uk"=>"", "en"=>"", "ru"=>""}, null: false
     t.json "short_body", default: {"uk"=>"", "en"=>"", "ru"=>""}, null: false
